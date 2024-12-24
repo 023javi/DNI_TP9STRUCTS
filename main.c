4bc6b07892aec73fc7b8bfa7c27b1c0b34c27a2d
Javier de la Cruz Sánchez <javier023javi@gmail.com>
2024-12-24 16:22:57 +0100

    feat: create rand_DNI_to_str function

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>1
+#include <string.h>
 #include <time.h>
+
+#define A 80
 
 typedef struct {
     unsigned dia, mes, anyo;
@@ -72,10 +74,57 @@ void rand_nom_comp(char nom_comp[20+1]) {
     strcat(nom_comp, nombres[rand_name]);
 }
 
+void rand_dig(char *dig) {
+    *dig = rand() % 10 + '0';
+}
+
+void rand_DNI(int *DNI) {
+    char dig;
+    *DNI = 0;
+    for (int i = 0; i < 8; i++) {
+        rand_dig(&dig);
+        *DNI = *DNI * 10 + (dig - '0');
+    }
+}
+
+unsigned resto_DNI(unsigned DNI) {
+    return DNI % 23;
+}
+
+char letra_calculada(unsigned restoDNI) {
+    char letra[] = "TRWAGMYFPDXBNJZSQVHLCKE";
+    return letra[restoDNI];
+}
+
+void rand_DNI_to_str(char *dnic[10+1]) {
+    int dni = 0;
+    rand_DNI(&dni); // Generar el DNI
+    int resto = resto_DNI(dni); // Calcular el resto
+    char letra[2]; // Espacio para la letra + terminador nulo
+
+    itoa(dni, dnic, 10); // Convertir el DNI a cadena
+    letra[0] = letra_calculada(resto); // Asignar la letra calculada
+    letra[1] = '\0'; // Asegurar el terminador nulo
+
+    strcat(dnic, letra); // Concatenar la letra al final del DNI
+}
+
+void rand_ALUMNO(struct ALUMNO *alumno) {
+    rand_DNI_to_str(alumno->DNI);
+    rand_nom_comp(alumno->nom_comp); // Generar el nombre completo aleatorio
+    rand_DATE(&alumno->nac); // Generar la fecha de nacimiento aleatoria
+}
 
 
-int main(void)
-{
+
+int main(void) {
     srand(time(NULL));
+    struct ALUMNO alumnos[A];
+
+    for (int i = 0; i < A; i++) {
+        rand_ALUMNO(&alumnos[i]);
+        print_ALUMNO(alumnos[i]);
+    }
+
     return 0;
 }
